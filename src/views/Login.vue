684a151f076f93b74c35db27197b4a68b803ae1c
DorijanKom <dorijan.komsic@stu.ibu.edu.ba>
2023-09-29 02:57:49 +0200

    Added form validation to Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,13 +8,17 @@
                         <h3 class="mb-5">Sign in</h3>
 
                         <div class="form-outline mb-4">
-                            <input v-model="email" type="email" id="typeEmailX-2" class="form-control form-control-lg" />
+                            <input v-model="email" @input="validateEmail" type="email" id="typeEmailX-2"
+                                class="form-control form-control-lg" :class="{ 'is-invalid': !emailValid }" />
+                            <div v-if="!emailValid" class="invalid-feedback">Invalid email format.</div>
                             <label class="form-label" for="typeEmailX-2">Email</label>
                         </div>
 
                         <div class="form-outline mb-4">
-                            <input v-model="password" type="password" id="typePasswordX-2"
-                                class="form-control form-control-lg" />
+                            <input v-model="password" @input="validatePassword" type="password" id="typePasswordX-2"
+                                class="form-control form-control-lg" :class="{ 'is-invalid': !passwordValid }" />
+                            <div v-if="!passwordValid" class="invalid-feedback">Password must be at least 6 characters.
+                            </div>
                             <label class="form-label" for="typePasswordX-2">Password</label>
                         </div>
 
@@ -23,8 +27,8 @@
                     </div>
                     <div class="card-footer">
                         <div class="text-center">
-                        <p>Don't have an account? <router-link to="/register">Register here</router-link></p>
-                    </div>
+                            <p>Don't have an account? <router-link to="/register">Register here</router-link></p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -41,25 +45,41 @@ export default {
         return {
             email: '',
             password: '',
+            emailValid: true,
+            passwordValid: true,
         };
     },
 
     methods: {
+
+        validateEmail() {
+            const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+            this.emailValid = emailRegex.test(this.email);
+        },
+
+        validatePassword() {
+            this.passwordValid = this.password.length >= 5;
+        },
+
         async login() {
-            try {
-                const response = await axios.post('api/user/login', {
-                    email: this.email,
-                    password: this.password,
-                })
-                if (response.status === 200) {
-                    this.$store.dispatch('login')
-                    this.$router.push('/dashboard');
-                } else {
+            if (this.emailValid && this.passwordValid) {
+                try {
+                    const response = await axios.post('api/user/login', {
+                        email: this.email,
+                        password: this.password,
+                    })
+                    if (response.status === 200) {
+                        this.$store.dispatch('login')
+                        this.$router.push('/dashboard');
+                    } else {
+                        alert('Invalid credentials. Please try again.');
+                    }
+                } catch (error) {
                     alert('Invalid credentials. Please try again.');
+                    console.error('Login error:', error)
                 }
-            } catch (error) {
-                alert('Invalid credentials. Please try again.');
-                console.error('Login error:', error)
+            } else {
+                alert("Please fix the form errors before submitting.");
             }
         }
     }
